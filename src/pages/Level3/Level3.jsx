6142ac1c72d4deeefac79895fba2dbad4261aa1d
kiiroiiruka <127270769+kiiroiiruka@users.noreply.github.com>
2025-06-24 04:59:34 +0900

    レベル1~2までの画面構築した際の様子をコミット(修正版4)

--- src/pages/Level3/Level3.jsx
+++ src/pages/Level3/Level3.jsx
@@ -1,19 +1,8 @@
-import { useNavigate } from 'react-router-dom'
-import './Level3.css'
-
 const Level3 = () => {
-  const navigate = useNavigate()
-
-  const handleClick = () => {
-    navigate('/')
-  }
-
   return (
-    <div className="level3-container">
-      <button onClick={handleClick}>レベル3へ</button>
-      <button onClick={handleClick}>レベル3へ</button>
+    <div>
+
     </div>
   )
 }
-
 export default Level3
